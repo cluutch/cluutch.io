1ae8087c744707e4b4e099a028c3e35116a85271
Cluutch API <code@cluutch.io>
2021-01-30 14:27:38 -0600

    feat: added meta tags for SEO

--- Gemfile
+++ Gemfile
@@ -29,6 +29,8 @@ gem 'jbuilder', '~> 2.7'
 gem 'bootstrap', '~> 5.0.0.beta1'
 gem 'jquery-rails'
 
+gem 'meta-tags'
+
 # Reduces boot times through caching; required in config/boot.rb
 gem 'bootsnap', '>= 1.4.4', require: false
 gem 'rexml'
--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,4 +1,7 @@
 class PagesController < ApplicationController
   def home
+    @page_title       = 'API for weed prices'
+    @page_description = 'An API for the price of weed/marijuana, pulling from legal dispensaries and the darknet. Long term crypto oracle.'
+    @page_keywords    = 'Weed, API, Marijuana, Crypto, Oracle, Market, Price'
   end
 end
