06d71a0b900f6fb95279cc3986205f8021e67915
Cluutch API <code@cluutch.io>
2021-01-30 14:58:41 -0600

    feat: link daily price to API

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -3,5 +3,8 @@ class PagesController < ApplicationController
     @page_title       = 'API for weed prices'
     @page_description = 'An API for the price of weed/marijuana, pulling from legal dispensaries and the darknet. Long term crypto oracle.'
     @page_keywords    = 'Weed, API, Marijuana, Crypto, Oracle, Market, Price'
+
+    newest_quote = Quote.all.order(date: :desc).first
+    @price_today = newest_quote.mean_price_per_oz.round(2)
   end
 end
