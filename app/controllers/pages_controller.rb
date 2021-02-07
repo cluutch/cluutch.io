41a3d952a716d70900008794ed51d01b419db7f2
Cluutch API <code@cluutch.io>
2021-02-07 09:29:52 -0600

    fix: don't fail if no quotes defined

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -5,6 +5,6 @@ class PagesController < ApplicationController
     @page_keywords    = 'Weed, API, Marijuana, Crypto, Oracle, Market, Price'
 
     newest_quote = Quote.all.order(date: :desc).first
-    @price_today = newest_quote.mean_price_per_oz.round(2)
+    @price_today = newest_quote ? newest_quote.mean_price_per_oz.round(2) : -1
   end
 end
