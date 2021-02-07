1b64dd0855d44f630e4129680b79a58f7f1bc5b9
Cluutch API <code@cluutch.io>
2021-02-07 09:27:49 -0600

    feat: require http auth from ApplicationController

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,10 @@
 class ApplicationController < ActionController::Base
+  before_action :http_authenticate, only: [:new, :create, :edit, :update, :destroy]
+ 
+  def http_authenticate
+    return true unless Rails.env == 'production'
+    authenticate_or_request_with_http_basic do |username, password|
+        username == ENV['HTTP_AUTH_USERNAME'] && password == ENV['HTTP_AUTH_PASSWORD']
+    end
+  end
 end
