3f949873df6d4fcf25bcb59746776f079ad4b2ee
Cluutch API <code@cluutch.io>
2021-03-03 09:03:32 -0600

    feat: added rspec with trivial assertion

--- Gemfile
+++ Gemfile
@@ -39,6 +39,7 @@ gem 'rexml'
 group :development, :test do
   # Call 'byebug' anywhere in the code to stop execution and get a debugger console
   gem 'byebug', platforms: [:mri, :mingw, :x64_mingw]
+  gem 'rspec-rails', '~> 4.0.2'
 end
 
 group :development do
---
+++ spec/models/v2/quote.rb
@@ -0,0 +1,9 @@
+require 'rails_helper'
+
+RSpec.describe 'V2::Quote' do           
+  it 'can be cloned with all values except price and date' do
+    # jurisdiction = V2::Jurisdiction.new
+    # original = V2::Quote.new date: 2.years.ago, 
+    expect(false).to_be true
+  end
+end
